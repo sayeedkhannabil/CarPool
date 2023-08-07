fc12e3bf582d36b3c01fbbeacef678388f245b11
sayeedkhannabil <sayeedkhannabil@gmail.com>
2023-08-06 19:29:50 -0500

    Search is added on components

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavMod from "./NavMod";
+import SearchComponent from "./SearchComponent"; 
 
 export default function Home() {
     return (
         <div>
         <NavMod />
+        <SearchComponent />
         </div>
     );
 }
---
+++ client/src/components/SearchComponent.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Form, Row, Col, Button, InputGroup, FormControl } from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const SearchComponent = () => {
+  const [fromLocation, setFromLocation] = useState('');
+  const [toLocation, setToLocation] = useState('');
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [passengerCount, setPassengerCount] = useState(1);
+
+  const handleSearch = () => {
+    const searchParams = {
+      fromLocation,
+      toLocation,
+      selectedDate,
+      passengerCount,
+    };
+
+    console.log(searchParams);
+    // Store the search parameters or make a request with them
+  };
+
+  return (
+    <Form>
+      <Row>
+        <Col>
+          <Form.Control type="text" placeholder="Leaving from" value={fromLocation} onChange={(e) => setFromLocation(e.target.value)} />
+        </Col>
+        <Col>
+          <Form.Control type="text" placeholder="Going to" value={toLocation} onChange={(e) => setToLocation(e.target.value)} />
+        </Col>
+        <Col>
+          <Form.Label>Date</Form.Label>
+          <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)} />
+        </Col>
+        <Col>
+          <InputGroup>
+            <Button variant="outline-secondary" onClick={() => setPassengerCount(Math.max(passengerCount - 1, 1))}>-</Button>
+            <FormControl value={passengerCount} readOnly />
+            <Button variant="outline-secondary" onClick={() => setPassengerCount(passengerCount + 1)}>+</Button>
+          </InputGroup>
+        </Col>
+        <Col>
+          <Form.Label>&nbsp;</Form.Label>
+          <Button variant="primary" onClick={handleSearch}>Search</Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default SearchComponent;
+
