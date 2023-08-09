edb7cd8942e81ae20f507d3ae9f17d9e7136a096
nomanaa <abdullahalnoman127@gmail.com>
2023-08-08 21:17:33 -0500

    article section added and placed the search componenet in the center

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavMod from "./NavMod";
 import SearchComponent from "./SearchComponent"; 
+import Article from "./Article"; 
 
 export default function Home() {
     return (
         <div>
         <NavMod />
         <SearchComponent />
+        <Article />
         </div>
     );
 }
