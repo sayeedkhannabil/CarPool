b2d89b370c575cdc8d447707803348315cd8b4ee
sayeedkhannabil <sayeedkhannabil@gmail.com>
2023-08-10 01:24:30 -0500

    Search component modified. Used container and added dynamic functionalities

--- client/src/components/SearchComponent.jsx
+++ client/src/components/SearchComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Row, Col, Button, InputGroup, FormControl } from 'react-bootstrap';
+import { Form, Row, Col, Button, InputGroup, FormControl, Container } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -22,16 +22,16 @@ const SearchComponent = () => {
   };
 
   return (
-    <div className="search-component">
+    <Container>
     <Form>
-      <Row>
-        <Col>
+      <Row className="align-items-center">
+        <Col xs={12} md={2}>
           <Form.Control type="text" placeholder="Leaving from" value={fromLocation} onChange={(e) => setFromLocation(e.target.value)} />
         </Col>
-        <Col>
+        <Col xs={12} md={2}>
           <Form.Control type="text" placeholder="Going to" value={toLocation} onChange={(e) => setToLocation(e.target.value)} />
         </Col>
-        <Col>
+        <Col xs={12} md={2}>
         <Form.Label></Form.Label>
           <DatePicker
             selected={selectedDate}
@@ -40,20 +40,20 @@ const SearchComponent = () => {
             customInput={<Form.Control as="input" />}
           />
         </Col>
-        <Col>
+        <Col xs={12} md={2}>
           <InputGroup>
             <Button variant="outline-secondary" onClick={() => setPassengerCount(Math.max(passengerCount - 1, 1))}>-</Button>
             <FormControl value={passengerCount} readOnly />
             <Button variant="outline-secondary" onClick={() => setPassengerCount(passengerCount + 1)}>+</Button>
           </InputGroup>
         </Col>
-        <Col>
+        <Col xs={12} md={2}>
           <Form.Label>&nbsp;</Form.Label>
           <Button variant="primary" onClick={handleSearch}>Search</Button>
         </Col>
       </Row>
     </Form>
-    </div>
+    </Container>
   );
 };
 
